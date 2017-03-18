4182e5a58d5464d1259de84d9462109e75e581ea
kotborealis <kotborealis@gmail.com>
2017-03-18 11:03:14 +0300

    Refactoring

--- index.js
+++ index.js
@@ -2,17 +2,16 @@
 const fs = require('fs');
 const konachan_dl = require("./konachan_dl");
 
-if(process.argv.length<5){
-    console.log("konachan_dl /path/to/save/dir/ tag starting_page");
+if(process.argv.length < 5){
+    console.log("usage: konachan_dl /path/to/save/dir/ tag starting_page");
 }
 else{
-    const path = process.argv[2].toString();
+    const [path, tag] = process.argv.slice(2);
     fs.accessSync(path,fs.constants.W_OK);
-    const tag = process.argv[3].toString();
 
     let starting_page = parseInt(process.argv[4]);
     if(Number.isNaN(starting_page))
         starting_page = 0;
-    
-    konachan_dl(path,tag,starting_page);
+
+    konachan_dl(path, tag, starting_page);
 }
--- konachan_dl.js
+++ konachan_dl.js
@@ -1,42 +1,45 @@
 'use strict';
 const fs = require('fs');
+const join_path = require('path').join;
 const request = require('request');
 
-function konachanPageImages(tag,page,cb){
+function konachanPageImages(tag, page, cb){
     const url = `http://konachan.net/post.json?tags=${tag}&page=${page}`;
-    request(url,(err,res,body)=>{
-        if(err || res.statusCode!== 200){
+    request(url, (err, res, body) => {
+        if(err || res.statusCode !== 200){
             throw new Error("Error loading page" + url);
         }
-        const images = JSON.parse(body).map(i=>i.file_url);
+        const images = JSON.parse(body).map(i => i.file_url);
         cb(images);
     });
 }
 
-function konachanPageSaveImages(path,tag,page){
+function konachanPageSaveImages(path, tag, page){
     console.log(`[Page ${page}] ${tag}`);
-    const cb = (images)=>{
-        const loop = (i)=>{
-            if(i==images.length){
-                konachanPageSaveImages(path,tag,page+1);
+    const cb = (images) => {
+        const loop = (i) => {
+            if(i === images.length){
+                konachanPageSaveImages(path, tag, page + 1);
             }
             else{
-                const image_name = decodeURI(images[i].split('/').pop());
+                const image = images[i];
+                const image_name = decodeURI(image.split('/').pop()).replace(/"|'/g, '~');
                 console.log(`[Page ${page}] Saving ${image_name}`);
                 try{
-                    fs.accessSync(path+image_name,fs.constants.F_OK);
+                    fs.accessSync(path + image_name, fs.constants.F_OK);
                     console.log(`[Page ${page}] ${image_name} already exists, skipping`);
-                    loop(i+1);
+                    loop(i + 1);
                 }
                 catch(e){
-                    const stream = request('http:' + images[i]).pipe(fs.createWriteStream(path+image_name.replace(/"|'/g, '~')));
-                    stream.on('finish',()=>loop(i+1));
+                    const file_path = join_path(`${path}/${image_name}`);
+                    const stream = request('http:' + image).pipe(fs.createWriteStream(file_path));
+                    stream.on('finish',() => loop(i + 1));
                 }
             }
         };
         loop(0);
     };
-    konachanPageImages(tag,page,cb);
+    konachanPageImages(tag, page, cb);
 }
 
 module.exports = konachanPageSaveImages;
